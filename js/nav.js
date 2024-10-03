0f4a32baf814974c395f58c5c7f92d4520763349
Monde Sineke <nsineke@gmail.com>
2024-10-03 13:45:55 +0200

    refactor: Clean up css

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,13 @@
-document.querySelectorAll('[data-btn="nav-toggle"]').forEach((i) => {
-  i.addEventListener("click", () => {
-    document.querySelector("#js-nav").classList.toggle("is-open");
+/*
+ * NOTE: This code opens and closes the html element
+ * that has an ID="js-nav"
+ * when and html element with the data attribute
+ * of [data-btn="nav-toggle"] is clicked
+ */
+if (document.querySelector('[data-btn="nav-toggle"]')) {
+  document.querySelectorAll('[data-btn="nav-toggle"]').forEach((i) => {
+    i.addEventListener("click", () => {
+      document.querySelector("#js-nav").classList.toggle("is-open");
+    });
   });
-});
+}
